bd21545aeb6759a7a3a8eed5f749ab5298bcde2c
Dan Alin <danalin06@gmail.com>
2024-11-20 02:46:43 +0100

    feat: toggle expand/shrink section 2

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,13 +3,23 @@
 import Card from '@/components/card';
 import Dialog from '@/components/dialog';
 import { flipWrapStore } from '@/stores/current-card.store';
-import { motion } from 'motion/react';
-import { useEffect, useState } from 'react';
+import { cn } from '@/utils/cn';
+import { AnimatePresence, motion } from 'motion/react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Detail() {
 	const [sectionThree, setSectionThree] = useState(false);
 	const [sectionFour, setSectionFour] = useState(false);
 	const [sectionFive, setSectionFive] = useState(false);
+
+	const [sectionTwoExpanded, setSectionTwoExpanded] = useState<boolean | undefined>();
+	const [isAnimating, setIsAnimating] = useState(false);
+
+	const sectionTwoRef = useRef<HTMLDivElement>(null);
+	const gridRef = useRef<HTMLDivElement>(null);
+
+	const [x, setX] = useState(0);
+	const [y, setY] = useState(0);
 
 	useEffect(() => {
 		flipWrapStore.flippedId = -1;
@@ -32,8 +42,43 @@ export default function Detail() {
 		}
 	};
 
+	const handleExpand = () => {
+		//get the position to center the card
+
+		const card = sectionTwoRef.current;
+		const grid = gridRef.current;
+		if (!card || !grid) return;
+		setIsAnimating(true);
+		setSectionTwoExpanded(sectionTwoExpanded ? false : true);
+
+		const { top, left, width, height } = card.getBoundingClientRect();
+		const { width: gridWidth, height: gridHeight } = grid.getBoundingClientRect();
+
+		const x = gridWidth / 2 - width / 2 - left;
+
+		//center the card vertically
+		const y = gridHeight / 2 - height / 2 - top;
+
+		setX(x);
+		setY(y);
+	};
+
 	return (
-		<motion.div className="grid grid-rows-12 grid-cols-12 gap-4 items-center justify-items-center  h-full p-8   ">
+		<motion.div
+			ref={gridRef}
+			className="grid grid-rows-[repeat(12,40px)] grid-cols-12 gap-4 items-center justify-items-center  h-full p-8   "
+		>
+			<AnimatePresence>
+				{sectionTwoExpanded && !isAnimating && (
+					<motion.div
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 0.5 }}
+						exit={{ opacity: 0 }}
+						className=" h-full w-full absolute bg-gray-900/30 z-5"
+					></motion.div>
+				)}
+			</AnimatePresence>
 			<motion.div
 				initial={{ opacity: 0, translateY: -100 }}
 				animate={{ opacity: 1, translateY: 0 }}
@@ -43,12 +88,33 @@ export default function Detail() {
 				<Card title="section 1" />
 			</motion.div>
 			<motion.div
+				ref={sectionTwoRef}
+				onClick={handleExpand}
 				initial={{ opacity: 0, translateX: 100 }}
-				animate={{ opacity: 1, translateX: 0 }}
-				transition={{ duration: 0.5, delay: 1 }}
-				className=" h-full w-full col-span-9 row-span-8"
+				animate={{
+					opacity: 1,
+					translateX: sectionTwoExpanded ? x : 0,
+					translateY: y,
+					height: sectionTwoExpanded ? '85dvh' : '100%',
+					width: sectionTwoExpanded ? '97dvw' : '100%',
+					zIndex: sectionTwoExpanded ? 10 : 1
+				}}
+				onAnimationComplete={() => {
+					setIsAnimating(false);
+				}}
+				transition={{
+					duration: 0.5,
+					delay: sectionTwoExpanded !== undefined ? 0 : 1,
+					zIndex: { delay: 0.5 }
+				}}
+				className={cn(
+					' h-full w-full col-span-9 row-span-8 cursor-pointer',
+					isAnimating && 'pointer-events-none'
+				)}
 			>
-				<Card title="section 2" />
+				<Card title="section 2">
+					<p className="text-sm text-gray-500">Click this card to expand/shrink </p>
+				</Card>
 			</motion.div>
 			<motion.div
 				initial={{ opacity: 0, translateY: 100 }}
